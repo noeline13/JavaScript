9d2456b85dd77fa3ad62222e40918485724336d5
Noeline <noeline.ravoniarisoa7@gmail.com>
2022-04-14 16:46:50 +0300

    some change

--- functions_exercise/keep-it-quiet.js
+++ functions_exercise/keep-it-quiet.js
@@ -1,8 +1,8 @@
-let keepItQuiet = function(str) {
-    let lowercase = str.toLowerCase("");
-    return lowercase + " ...";
-};
+ function keepItQuiet(str) {
+     var lowercase = str.toLowerCase("");
+     return lowercase + " ...";
+ }
 
-console.log(keepItQuiet("HOORAY")); // 'hooray...' (en anglais)
-console.log(keepItQuiet("Doggo")); // 'doggo...' (en anglais)
-console.log(keepItQuiet("WHAT ?!?!")); // 'what ?!?!...' (quoi ?!!...)
+ console.log(keepItQuiet("HOORAY")); // 'hooray...' (en anglais)
+ console.log(keepItQuiet("Doggo")); // 'doggo...' (en anglais)
+ console.log(keepItQuiet("WHAT ?!?!")); // 'what ?!?!...' (quoi ?!!...)
