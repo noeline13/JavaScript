c4ef21587549ade038f3f8b4d349e4aff2c82b9f
Noeline <noeline.ravoniarisoa7@gmail.com>
2022-04-20 09:01:55 +0300

    key pair

--- objects_exercise/key-pair.js
+++ objects_exercise/key-pair.js
@@ -1,3 +1,16 @@
 function keyPair(obj1, obj2, key) {
-
+    let arr = [];
+    arr.push(obj1[key]);
+    arr.push(obj2[key]);
+    return arr;
 }
+
+let cat1 = { name: 'jinkee', breed: 'calico' };
+let cat2 = { name: 'garfield', breed: 'red tabby' };
+
+console.log(keyPair(cat1, cat2, 'breed')); // [ 'calico', 'red tabby' ]
+console.log(keyPair(cat1, cat2, 'name')); // [ 'jinkee', 'garfield ]
+
+let sport1 = { name: "volleyball", team: true };
+let sport2 = { name: "golf", team: false };
+console.log(keyPair(sport1, sport2, 'team')); // [ true, false ]
